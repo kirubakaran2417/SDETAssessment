0b11bd2ee7d0f590105f1202a03b2b75e89030b7
kirubakaran <k.kirubakaran@stackroute.in>
2024-05-03 20:41:32 +0530

    account

--- src/main/java/account/Account.java
+++ src/main/java/account/Account.java
@@ -1,4 +1,71 @@
 package account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+    private List<String> transactions;
+
+    public Account(String accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(String transactionId, double amount) {
+        balance += amount;
+        transactions.add(transactionId);
+    }
+
+    public void withdraw(String transactionId, double amount) {
+        balance -= amount;
+        transactions.add(transactionId);
+    }
+
+    public String produceStatement() {
+        StringBuilder statement = new StringBuilder();
+        statement.append("Name: ").append(accountName).append("\n");
+        statement.append("Account: ").append(accountNumber).append("\n");
+        statement.append("Balance: ").append(String.format("%.2f", balance)).append("\n");
+        for (String transaction : transactions) {
+            statement.append(transaction).append("\n");
+        }
+        return statement.toString();
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<String> transactions) {
+        this.transactions = transactions;
+    }
 }
--- src/test/java/glue/AccountSteps.java
+++ src/test/java/glue/AccountSteps.java
@@ -3,6 +3,11 @@ package glue;
 import account.Account;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class AccountSteps {
 
@@ -10,7 +15,38 @@ public class AccountSteps {
 
     @Given("^Account exists for Acc No\\. \"([^\"]*)\" with Name \"([^\"]*)\"$")
     public void accountExistsForAccNoWithName(String number, String name) {
-        throw new PendingException();
+        account = new Account(number, name);
     }
 
+    @Given("^deposits are made$")
+    public void depositsAreMade(List<List<String>> deposits) {
+        for (List<String> deposit : deposits) {
+            String transactionId = deposit.get(0);
+            double amount = Double.parseDouble(deposit.get(1));
+            account.deposit(transactionId, amount);
+        }
+    }
+
+    @Given("^withdrawls are made$")
+    public void withdrawalsAreMade(List<List<String>> withdrawals) {
+        for (List<String> withdrawal : withdrawals) {
+            String transactionId = withdrawal.get(0);
+            double amount = Double.parseDouble(withdrawal.get(1));
+            account.withdraw(transactionId, amount);
+        }
+    }
+
+    @When("^statement is produced$")
+    public void statementIsProduced() {
+        String statement = account.produceStatement();
+        System.out.println("Generated Statement:\n" + statement);
+        Assert.assertTrue(statement.contains("Name: Bob Smith"));
+        Assert.assertTrue(statement.contains("Account: 12345678"));
+
+    }
+
+    @Then("^statement includes \"([^\"]*)\"$")
+    public void statementIncludes(String expectedContent) {
+        Assert.assertTrue(account.produceStatement().contains(expectedContent));
+    }
 }
