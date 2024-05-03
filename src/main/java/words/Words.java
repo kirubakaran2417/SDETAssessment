29b44375a817ee307aedd699020021894e49600d
kirubakaran <k.kirubakaran@stackroute.in>
2024-05-03 17:42:45 +0530

    develop

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,9 +1,13 @@
 package words;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(sentence.toLowerCase().split("\\W+"))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
